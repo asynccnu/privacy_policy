cf8edd1062544364b1027bd8f7a04391ca548147
HumbertZhang <504490160@qq.com>
2020-01-18 22:02:07 +0800

    rename path

--- server.py
+++ server.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def privacy():
     return send_file("build/index.html")
 
+@app.route("/privacy_static/<path:filename>")
+def privacy_static(filename):
+    if filename.startswith("/"):
+        return send_file("privacy_static" + filename)
+    else:
+        return send_file("privacy_static/" + filename)
+
 if __name__ == '__main__':
     app.run()
